7669454badcab0990a5992ee4f5f5319f43963f2
Gus Robinson <gus.robinson@yoti.com>
2024-03-14 11:42:35 +0000

    Look for gpiochip4

--- astroberry_focuser.cpp
+++ astroberry_focuser.cpp
@@ -953,7 +953,7 @@ IPState AstroberryFocuser::MoveAbsFocuser(uint32_t targetTicks)
 
     accellerationTime = FocusStepDelayN[0].value * 20;
 	if (accellerationTime > 10000) {
-		accellerationTime = 10000
+		accellerationTime = 10000;
 	}
 
 
